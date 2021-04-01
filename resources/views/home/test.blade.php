6622ae1a46a7b36707dd965804032dd120f77fbe
maga62 <muhammad709059952@gmail.com>
2021-04-01 22:49:42 +0300

    first commit

---
+++ resources/views/home/test.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Laravel test page</title>
+</head>
+<body>
+
+<h1>laravel test page</h1>
+Id no:{{$id}} <br>
+Name: {{$name}}
+<br>
+
+<a href="{{route('home')}}">Ana sayfa</a>
+
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/eskihome', function () { // root cağırma şekli / belirtib ardından çalışacak fonk yada controller cağrıla bilir
+    return view('welcome');
+
+});
+Route::redirect('/anasayfa', '/home')->name('anasayfa');// anasayfa yazılınca direk home gidilir
+
+
+Route::get('/', function () { // root cağırma şekli 1. yontem / belirtib ardından çalışacak fonk yada controller cağrıla bilir
+    return view('home.index',); //['name' => 'Muhammed Muhammedov'] içerde değişken şeklinde göndere biliriz ve index.blade.php de {{ $name }} şeklinde çağıra biliriz
+});
+
+
+Route::get('test/{id}/{name}', [HomeController::class, 'test'])->whereNumber('id')->whereAlpha('name')->name('test');
+Route::get('/home', [HomeController::class, 'index'])->name('home'); // root cağırma şekli 2. yontem
+Route::get('/aboutus', [HomeController::class, 'aboutus'])->name('aboutus');
+
+
+
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
